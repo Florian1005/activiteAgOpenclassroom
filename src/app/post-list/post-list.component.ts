42e6ff3e22b2c7dfe7ba8d2f7d684f82f60ef54c
Florian1005 <43569972+Florian1005@users.noreply.github.com>
2019-10-08 14:49:38 +0200

    Update post-list.component.ts

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -26,9 +26,9 @@ export class PostListComponent implements OnInit {
     this.loveIts = this.loveIts - 1;
   }
   getColor() {
-    if (this.loveIts > 1) {
+    if (this.loveIts > 0) {
       return 'rgb(0, 255, 64,0.40)';
-    } else if (this.loveIts < 1 ) {
+    } else if (this.loveIts < 0 ) {
       return 'rgba(220,53,69,0.36)';
     }
   }
